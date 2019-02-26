55ef8667ef8d6988e9edae4dbe7cb7e1217a9bc1
Dan Draper <dan@coderdan.co>
2019-02-26 17:47:20 +1100

    Added test

--- test/alive_test.exs
+++ test/alive_test.exs
@@ -1,8 +1,31 @@
 defmodule AliveTest do
   use ExUnit.Case
-  doctest Alive
 
-  test "greets the world" do
-    assert Alive.hello() == :world
+  test "#comtemporaries" do
+    assert Person.contemporaries() == [
+      [
+        %Person{born: 1642, died: 1726, name: "Newton"},
+        %Person{born: 1707, died: 1783, name: "Euler"},
+      ],
+      [
+        %Person{born: 1707, died: 1783, name: "Euler"},
+        %Person{born: 1749, died: 1827, name: "Laplace"},
+        %Person{born: 1768, died: 1830, name: "Fourier"},
+      ],
+      [
+        %Person{born: 1749, died: 1827, name: "Laplace"},
+        %Person{born: 1768, died: 1830, name: "Fourier"},
+        %Person{born: 1815, died: 1852, name: "Lovelace"}
+      ],
+      [
+        %Person{born: 1858, died: 1947, name: "Planck"},
+        %Person{born: 1867, died: 1934, name: "Curie"},
+        %Person{born: 1912, died: 1954, name: "Turing"}
+      ],
+      [
+        %Person{born: 1955, died: 2011, name: "Jobs"},
+        %Person{born: 1984, died: nil, name: "Zuckerberg"}
+      ]
+    ]
   end
 end
